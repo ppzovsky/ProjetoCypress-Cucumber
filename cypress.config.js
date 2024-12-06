0c167c99b1b1e80ddb486cbf806cf5fe96674c51
João Pedro Soares <joaopedrosoaresdebrito@gmail.com>
2024-12-06 09:44:14 -0300

    push: ajuste nas configuraçoes do cypress implementação do mochawesome report

--- cypress.config.js
+++ cypress.config.js
@@ -4,11 +4,19 @@ const { defineConfig } = require("cypress");
 module.exports = defineConfig({
   e2e: {
     setupNodeEvents(on, config) {
-      on('file:preprocessor', cucumber())
+      on('file:preprocessor', cucumber());
     },
     specPattern: [
       'cypress/e2e/cucumber_tests/*.feature',
       'cypress/e2e/*.cy.js'
-    ]
+    ],
+    reporter: "mochawesome",
+    reporterOptions: {
+      reportDir: "cypress/report/mochawesome-report",
+      overwrite: false, 
+      html: true,       
+      json: true,       
+      timestamp: "mm-dd-yyyy_HH-MM" 
+    }
   },
 });
