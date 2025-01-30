a1eab3692e561802b680154455baf832890e02a0
João Pedro Soares <prestador-joao.brito@neurotech.com.br>
2025-01-30 13:01:10 -0300

    push: ajustes associacao de resultados para marcar o status do teste

--- utils/associarTests.js
+++ utils/associarTests.js
@@ -75,12 +75,8 @@ async function buscarCasosDeTeste() {
 async function associarResultados(testRunResults, testCases) {
   const url = `https://dev.azure.com/${org}/${project}/_apis/test/runs?api-version=7.1`;
 
-  for (let i = 0; i < testCases.length; i++) {
-    console.log(testCases[i].workItem.name);
-  }
   for (const result of testRunResults) {
     // Encontrar o caso de teste correspondente
-    console.log(result.automatedTestName)
     const testCase = testCases.find(tc => tc.workItem.name === result.automatedTestName);
 
     if (testCase) {
@@ -94,12 +90,12 @@ async function associarResultados(testRunResults, testCases) {
         const resultado = {
           name: result.testCaseTitle,
           automated: true,
+          build: {id: result.build.id, name: result.build.name, url: result.build.url},
           pointIds: [testPoint], 
           plan: {
             id: testCase.testPlan.id,
           },
-          state: result.state,
-          outcome: result.outcome,
+          state: 'InProgress',
           startedDate: now.toISOString(),
           completedDate: now.toISOString()
         };
@@ -118,6 +114,8 @@ async function associarResultados(testRunResults, testCases) {
 
           console.log(`Resultado do teste "${result.testCaseTitle}" associado com sucesso!`);
           console.log(response.data);
+          await atualizarResultadoTeste(response.data.id, testCase.workItem.id, result.outcome);
+          await finalizarTestRun(response.data.id, result.outcome);
         } catch (error) {
           console.error(`Erro ao associar resultado ao teste "${result.testCaseTitle}":`, error.response?.data || error.message);
         }
@@ -130,23 +128,38 @@ async function associarResultados(testRunResults, testCases) {
   }
 }
 
+//Funcao para atualizar o resultado do teste (se passou ou falhou)
+async function atualizarResultadoTeste(testRunId, testCaseId, result) {
+  const url = `https://dev.azure.com/${org}/${project}/_apis/test/Runs/${testRunId}/results?api-version=7.1-preview.6`;
+  console.log(result)
+  const body = [{
+    id: testCaseId,
+    outcome: result,
+  }];
+
+  try {
+    await axios.patch(url, body, { auth });
+  } catch (error) {
+    console.error('Erro ao atualizar teste:', error.response?.data || error.message);
+  }
+}
 
 //Função para retornar o status da execução ao completed
 async function finalizarTestRun(testRunId) {
   const url = `https://dev.azure.com/${org}/${project}/_apis/test/runs/${testRunId}?api-version=7.1-preview.3`;
   const body = {
     state: 'Completed',
+    comment: 'Resultado definido via API'
   };
 
   try {
-    await axios.patch(url, body, { auth });
-    console.log(`Test run ${testRunId} finalizado com sucesso.`);
+    const response = await axios.patch(url, body, { auth });
+    console.log(`Test run ${testRunId} finalizado com sucesso.`, response.data);
   } catch (error) {
     console.error('Erro ao finalizar o test run:', error.response?.data || error.message);
     throw error;
   }
 }
-
 
 async function main() {
   try {
@@ -160,13 +173,9 @@ async function main() {
 
     const testRunId = lastTestRun.id;
 
-    // Buscar resultados do test run 
+    //Buscar resultados do test run 
     console.log('Buscando resultados do test run...')
     const testRunResults = await buscarResultadosTestRun(testRunId);
-
-    //Muda o status do test run para fazer a associacao 
-    // console.log('Abrindo test run...')
-    // await reabrirTestRun(testRunId);
 
     // Buscar casos de teste no plano
     console.log('Buscando casos de teste...')
@@ -175,10 +184,6 @@ async function main() {
     // Associa os resultados aos casos de teste
     console.log('Associando resultados aos casos de teste...')
     await associarResultados(testRunResults, testCases);
-
-    //Finalizando test run
-    // console.log('Fechando execucao do teste')
-    // await finalizarTestRun(testRunId);
 
   } catch (error) {
     console.error('Erro no processo:', error.message);
